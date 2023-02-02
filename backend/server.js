ac17830e42e4f05a8be8647fa3d888eb12f74548
BertrandMuh <sangwabertrand14@gmail.com>
2023-02-02 12:09:41 -0600

    add gitignore

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+const express = require('express');
+
+
+const app = express()
+
+app.listen(5000, () => {
+    console.log('Running on port 5000');
+})
---
+++ client/src/components/card/index.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Card(props) {
+  return (
+    <div>
+      <p>Card</p>
+    </div>
+  )
+}
+
+export default Card
